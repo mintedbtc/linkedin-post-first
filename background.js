b2b5d59acf5de1a5427585ca8166cec8e1551572
Mini Me <minime@Minis-Mac-mini.attlocal.net>
2026-02-02 08:32:23 -0800

    Simplify background script - remove alarms

--- background.js
+++ background.js
@@ -3,35 +3,9 @@
 const STORAGE_KEY = 'lpf_last_post_date';
 const STREAK_KEY = 'lpf_streak';
 
-// Reset at midnight
-function scheduleReset() {
-  const now = new Date();
-  const tomorrow = new Date(now);
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  tomorrow.setHours(0, 0, 0, 0);
-  
-  const msUntilMidnight = tomorrow - now;
-  
-  // Set alarm for midnight
-  chrome.alarms.create('midnight-reset', {
-    when: Date.now() + msUntilMidnight,
-    periodInMinutes: 24 * 60 // Repeat daily
-  });
-}
-
-// Handle alarm
-chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === 'midnight-reset') {
-    // Don't reset the storage - just let the date check handle it
-    // This alarm is mainly for refreshing any open LinkedIn tabs
-    console.log('LinkedIn Post First: New day started');
-  }
-});
-
 // Initial setup
 chrome.runtime.onInstalled.addListener(() => {
   console.log('LinkedIn Post First installed');
-  scheduleReset();
   
   // Set default values
   chrome.storage.local.get([STREAK_KEY], (result) => {
@@ -39,11 +13,6 @@ chrome.runtime.onInstalled.addListener(() => {
       chrome.storage.local.set({ [STREAK_KEY]: 0 });
     }
   });
-});
-
-// Handle startup
-chrome.runtime.onStartup.addListener(() => {
-  scheduleReset();
 });
 
 // Listen for messages from content script
